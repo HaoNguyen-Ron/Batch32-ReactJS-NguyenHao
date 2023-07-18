cd8039a86b13057491407c691380491b80483405
HaoNguyen-Ron <hao100319@gmail.com>
2023-07-18 20:22:34 +0700

    update

---
+++ bt3/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import PlayList from './Components/PlayList';
+
+
+function App() { // = Home Page
+  return (
+      <PlayList />
+  );
+}
+
+export default App;
