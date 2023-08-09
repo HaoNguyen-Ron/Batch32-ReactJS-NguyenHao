3a03ab0586c5953eaf4be2b2e169edc7aa2ef26b
Saltymeatball <hao100319@gmail.com>
2023-08-09 11:08:09 +0700

    TODO list with Check & Delete

--- bt3/src/pages/todo.jsx
+++ bt3/src/pages/todo.jsx
@@ -2,15 +2,9 @@
 import Action from "Components/todo/action";
 import Add from "Components/todo/addMission";
 import List from "Components/todo/listMission";
+import 'Components/todo/todo.css'
+import ToDoApp from "Components/todo/todoapp";
 
-const TodoPage = () => {
-    return ( 
-        <>
-            <Add />
-            <List/>
-            <Action />
-        </>
-     );
-}
- 
+const TodoPage = () => <ToDoApp />
+
 export default TodoPage;
