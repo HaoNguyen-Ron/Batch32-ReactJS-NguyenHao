23588c48054958ce5a4a9b0494100234bacc05a9
Saltymeatball <hao100319@gmail.com>
2023-08-10 09:30:50 +0700

    Todo App final exercise

--- bt3/src/pages/todo.jsx
+++ bt3/src/pages/todo.jsx
@@ -1,13 +1,5 @@
 
-<<<<<<< HEAD
-import Action from "Components/todo/action";
-import Add from "Components/todo/addMission";
-import List from "Components/todo/listMission";
-=======
-import ToDoApp from "Components/Todo/todoapp";
-import 'Components/Todo/todoapp'
-
->>>>>>> 9d4dc8fdc676295919fb69f951c6f8ef0b83641b
+import ToDoApp from "Components/todo/todoapp";
 
 const TodoPage = () => <ToDoApp />
 
