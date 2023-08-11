1cb323384acf4b9381d8644662aeaeccf7a970e3
HaoNguyen-Ron <hao100319@gmail.com>
2023-08-11 19:09:17 +0700

    update some detail

--- bt3/src/Components/PlayList/play.jsx
+++ bt3/src/Components/PlayList/play.jsx
@@ -8,7 +8,7 @@ import React, { useCallback, useEffect, useRef, useState } from 'react';
 import './music.css';
 
 
-export default function Play({ title, artist, image, file, id, onPrev, onNext, onNextMusic }){
+export default function Play({ title, artist, image, file, id, onPrev, onNext, onNextMusic }) {
 
     const [isPlaying, setIsPlaying] = useState(false);
 
@@ -17,55 +17,56 @@ export default function Play({ title, artist, image, file, id, onPrev, onNext, o
     const [duration, setDuration] = useState(0);
 
     const audioRef = useRef();
-    
-    const onUpdateTimer = useCallback(() => {
-        setTimer(audioRef.current.currentTime);
-      }, []);
-
-    
-    const playPause = useCallback(()=>{
-        if(isPlaying){
-            audioRef.current.pause();
-        } else{
-            audioRef.current.play();
-        }
-        setIsPlaying((prevState)=>!prevState)
-    },[isPlaying],
-    );
-    
-    useEffect(()=>{
-        setIsPlaying(false);
-    },[id])
 
     useEffect(() => {
-        function getTrackLength() {
-          audioRef.current.addEventListener('loadedmetadata', function () {
-            setDuration(audioRef.current.duration);
-          });
-        }
-    
-        getTrackLength();
-    
-        // if (audioRef.current) {
-        //   getTrackLength(audioRef.current);
-        // }
+        setIsPlaying(false);
+    }, [id]);
+
+    const onUpdateTimer = useCallback(() => {
+        setTimer(audioRef.current.currentTime);
     }, []);
-    
+
+
+    const playPause = useCallback(() => {
+        if (isPlaying) {
+            audioRef.current.pause();
+        } else {
+            audioRef.current.play();
+        }
+        setIsPlaying((prev) => !prev);
+    }, [isPlaying],
+    );
+
+    const getTrackLength = () => {
+        audioRef.current.addEventListener('loadedmetadata', function () {
+            // Thân hàm
+            setDuration(audioRef.current.duration);
+        });
+    }
+    useEffect(() => {
+        getTrackLength();
+    }, []);
+
     const onChangeSlider = useCallback((event) => {
         audioRef.current.currentTime = event.target.value;
-      }, []);
+    }, []);
 
     const getDuration = (duration) => {
         const minutes = Math.floor(duration / 60); // Số phút
         const seconds = Math.floor(duration % 60); // Số giây
-      
+
         const formattedDuration = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-      
+
         return formattedDuration;
-    }
+    };
 
 
-    return (  
+    const onReloadMusic = () => {
+        audioRef.current.load();
+    };
+
+
+    return (
         <div className="rightside d-flex">
             <div className="info d-flex flex-column justify-content-start">
                 <h1>Now Playing</h1>
@@ -75,29 +76,29 @@ export default function Play({ title, artist, image, file, id, onPrev, onNext, o
 
             <div className="playingsong d-flex">
                 <div className="disc-wrapper">
-                    <div className={`music-disc ${isPlaying? 'music-disc-spin' :''}`} src={image}>
+                    <div className={`music-disc ${isPlaying ? 'music-disc-spin' : ''}`} src={image}>
                         <div className="center-circle" />
                     </div>
-                
+
                     <div className="song-title">
                         <h3>{title}</h3>
 
                         <p>{artist}</p>
                     </div>
 
-                    <audio 
+                    <audio
                         // controls
-                        id="song" 
-                        src={file} 
-                        type="audio/mpeg" 
-                        ref={audioRef} 
+                        id="song"
+                        src={file}
+                        type="audio/mpeg"
+                        ref={audioRef}
                         onTimeUpdate={onUpdateTimer}
                         onEnded={onNextMusic}
                     />
 
 
-                    <input 
-                        type="range" 
+                    <input
+                        type="range"
                         defaultValue={0}
                         max={duration}
                         value={timer}
@@ -113,22 +114,27 @@ export default function Play({ title, artist, image, file, id, onPrev, onNext, o
                 </div>
 
                 <div className="control-button d-flex">
-
+                    <div>
+                        <i class="fa-brands fa-mixer"></i>
+                    </div>
 
                     <div>
-                        <i className="fa-solid fa-backward" onClick={onPrev}/>
+                        <i className="fa-solid fa-backward" onClick={onPrev} />
                     </div>
 
                     <div onClick={playPause}>
                         {isPlaying ? (
                             <i className="fa-solid fa-pause fa-2xl"></i>
-                            ) : (
+                        ) : (
                             <i className="fa-solid fa-play fa-2xl"></i>
                         )}
                     </div>
 
                     <div>
-                    <i className="fa-solid fa-forward" onClick={onNext} />
+                        <i className="fa-solid fa-forward" onClick={onNext} />
+                    </div>
+                    <div>
+                        <i className="fa fa-refresh"  onClick={onReloadMusic} aria-hidden="true"></i>
                     </div>
 
                 </div>
@@ -136,4 +142,3 @@ export default function Play({ title, artist, image, file, id, onPrev, onNext, o
         </div>
     );
 }
- 
